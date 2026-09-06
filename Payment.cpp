d998a3cab6c946cd14aac91ec82c812492de60d5
009manish <manishdhoundiyal31@gmail.com>
2026-09-06 15:25:49 +0530

    Remove duplicate root source files

--- Payment.cpp
+++
@@ -1,9 +0,0 @@
-class Payment {
-protected:
-    double amount;
-public:
-    Payment() : amount(0.0) {}
-    virtual ~Payment() = default;
-    // ABSTRACTION: pure virtual function defines the payment contract.
-    virtual bool pay(double amount) = 0;
-};
